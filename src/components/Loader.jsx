47ab96206af3ad43441b890f6cd69664b2da9184
mohamadkurd <hsemohamad385@gmail.com>
2025-06-06 18:12:03 +0330

    add Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loader() {
+return (
+    <div className="flex items-center justify-center py-10">
+    <div className="w-10 h-10 border-4 border-blue-200 rounded-full border-t-blue-500 animate-spin"></div>
+    </div>
+);
+}
